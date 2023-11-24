4ada2a89e21a84ef4953577ed2510a40a54ec52e
Fireshadow111 <muhammadrajah16@gmail.com>
2023-11-24 12:35:22 +0200

    adding

--- script/index.js
+++ script/index.js
@@ -4,14 +4,13 @@ let minutesInput = document.getElementById('ageInput');
 let secondsOutput = document.getElementById('daysOutput');
 let calButton = document.getElementById('calButton');
 
-//Adding event listener
 calButton.addEventListener('click', 
 
-//Adding function
+//Adding function 
 function ageToDays() {
   let age = parseInt(ageInput.value);
 
-  //Adding validation
+  //Adding validation, taking inputed age and multipying by 365
   if (!isNaN(age)) {
     let Days = age * 365;
     daysOutput.value = Days;
